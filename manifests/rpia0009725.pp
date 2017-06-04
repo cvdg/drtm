540610447c646ebd932fd1b41a9736006c584018
Cees van de Griend <c.vande.griend@gmail.com>
2017-06-04 11:14:08 +0200

    fixed: oops

--- manifests/rpia0009725.pp
+++ manifests/rpia0009725.pp
@@ -1,5 +1,5 @@
 #
-# manifests/rpia0008101.pp
+# manifests/rpia0009725.pp
 #
 # Hardware:    Raspberry Pi 1
 # Memory:      512 KB
@@ -7,14 +7,14 @@
 # BogoMIPS:    38.40  / 0
 # Filesystem:  xx GB (microSD)
 #
-# Date:        2017-06-03
+# Date:        2017-06-04:
 # Description: MQTT server
 #
 # Author:      Cees van de Griend <c.vande.griend@gmail.com>
 # Copyright:   (c) 2017 C.A. van de Griend
 #
 
-node 'rpia0008101' {
+node 'rpia0009725' {
     include raspi
 
     include user::root
