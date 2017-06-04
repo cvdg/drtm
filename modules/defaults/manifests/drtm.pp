a4196c5d6b7506ed0400b7f30c850ec10f74ed06
Cees van de Griend <c.vande.griend@gmail.com>
2017-06-04 11:12:08 +0200

    Switched from cron to systemd

--- modules/defaults/manifests/drtm.pp
+++ modules/defaults/manifests/drtm.pp
@@ -1,7 +1,7 @@
 #
 # modules/defaults/manifests/drtm.pp
 #
-# Date:        2017-05-24
+# Date:        2017-06-04
 # Description: Dartmoor
 #
 # Author:      Cees van de Griend <c.vande.griend@gmail.com>
@@ -10,13 +10,35 @@
 
 class defaults::drtm {
     file { '/etc/cron.daily/drtm': 
-        source  => 'puppet:///modules/defaults/etc/cron.daily/drtm',
+        # source  => 'puppet:///modules/defaults/etc/cron.daily/drtm',
+        # owner   => 'root',
+        # group   => 'root',
+        # mode    => '0755',
+        ensure  => 'absent',
+    }
+
+    file { '/etc/cron.weekly/drtm': 
+        ensure  => 'absent',
+    }
+
+    file { '/etc/systemd/system/drtm-daily.serice':
+        source  => 'puppet:///etc/systemd/system/drtm-daily.serice',
         owner   => 'root',
         group   => 'root',
         mode    => '0755',
     }
 
-    file { '/etc/cron.weekly/drtm': 
-        ensure  => 'absent',
+    file { '/etc/systemd/system/drtm-daily.timer':
+        source  => 'puppet:///etc/systemd/system/drtm-daily.timer',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        require => File['/etc/systemd/system/drtm-daily.serice'],
+    }
+
+    service { 'drtm-daily',
+        enable  => 'true',
+        ensure  => 'started',
+        require => File['/etc/systemd/system/drtm-daily.timer'],
     }
 }
