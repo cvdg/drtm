519323345b1818d5cc145f914d43b1eb7635ba8f
Cees van de Griend <c.vande.griend@gmail.com>
2017-08-20 12:26:58 +0000

    New host: rpid0020615
    Signed-off-by: Cees van de Griend <c.vande.griend@gmail.com>

---
+++ bin/git-config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config --global push.default simple
+git config --global user.name "Cees van de Griend"
+git config --global user.email c.vande.griend@gmail.com
+
+git config --global commit.gpgsign true
+git config --global user.signingkey 89624C42
---
+++ manifests/rpid0020615.pp
@@ -0,0 +1,22 @@
+#
+# manifests/rpid0020615.pp
+#
+# Hardware:    Raspberry Pi 3
+# Memory:      1 GB
+# CPU:         armv7l / 4
+# BogoMIPS:    38.40  / 153.60
+# Filesystem:  xx GB (microSD)
+#
+# Date:        2017-06-06
+# Description: ??
+#
+# Author:      Cees van de Griend <c.vande.griend@gmail.com>
+# Copyright:   (c) 2017 C.A. van de Griend
+#
+
+node 'rpid0020615' {
+    include raspi
+
+    include user::root
+    include user::cvdg
+}
